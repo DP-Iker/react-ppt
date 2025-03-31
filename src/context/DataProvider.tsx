053cdb2cf06cbaf9a4c7730a02cc392826ab4018
Iker Diaz <diazp.iker@gmail.com>
2025-03-31 19:43:29 +0200

    Contextos creados

---
+++ src/context/DataProvider.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { DataContext } from "./DataContext";
+
+const DataProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [playerName, setPlayerName] = useState("Player");
+  const [image, setImage] = useState<string | null>(null);
+
+  return (
+    <DataContext.Provider value={{ playerName, setPlayerName, image, setImage }}>
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+export default DataProvider;
