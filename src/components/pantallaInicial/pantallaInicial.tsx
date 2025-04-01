d3e756fe441678ea3f49782b9e4102171ee7a048
Iker Diaz <diazp.iker@gmail.com>
2025-04-01 17:27:47 +0200

    Funcionalidad de pantalla inicial hecha

--- src/components/pantallaInicial/pantallaInicial.tsx
+++ src/components/pantallaInicial/pantallaInicial.tsx
@@ -1,51 +1,33 @@
-import { ChangeEvent, FC, ReactElement, useState } from "react";
+import { FC, ReactElement, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { DataContext } from "../../context/DataContext";
+import { handleImageChange } from "../../utils/handleImage"
 
 const Inicio: FC = (): ReactElement => {
+  const navigate = useNavigate();
+  const [nombre, setNombre] = useState("");
+  const dataContext = useContext(DataContext);
 
+  const handleClick = () => {
+    if (dataContext) {
+      dataContext.setPlayerName(nombre);
+      navigate("/juego");
+    }
+  };
 
-        const [nombre, setNombre] = useState("");
-        const [foto, setFoto] = useState<string | null>(null);
-
-        const escogerNombre = (e: ChangeEvent<HTMLInputElement>) => {
-        setNombre(e.target.value);
-        };
-
-        const escogerFoto = (e: ChangeEvent<HTMLInputElement>) => {
-            
-            const archivo = e.target.files?.[0];
-            if (archivo) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setFoto(reader.result as string);
+  return (
+    <div className="inicio">
+      <h2>Escribe tu nombre</h2>
+      <input
+        type="text"
+        placeholder="Tu nombre"
+        value={nombre}
+        onChange={(e) => setNombre(e.target.value)}
+      />
+      <input type="file" accept="image/*" onChange={(e) => handleImageChange(e, dataContext!.setImage)} />
+      <button onClick={handleClick}>Botón</button>
+    </div>
+  );
 };
 
-              reader.readAsDataURL(archivo); // Convierte a base64 para mostrar imagen
-            }
-};
-
-
-	
-return <div>
-<h2>Escribe tu nombre</h2>
-<input type="text" placeholder="Tu nombre"
-value= {nombre}
-onChange={escogerNombre}
-/>
-
-
-
-<h3>Sube tu foto:</h3>
-<input type="file" accept="image/*" onChange={escogerFoto} /><br />
-
-{foto && (
-<div>
-<p>¡Hola {nombre}!</p>
-<img src={foto} alt="Foto de perfil" width={150} />
-</div>
-)}
-</div>
-
-    };
-
-    
 export default Inicio;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import DataProvider from "./context/DataProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
