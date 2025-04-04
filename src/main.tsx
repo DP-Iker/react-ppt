4d39e0a6de4a68a96a41e1afab9e7868388afbce
nicolas <nicolasfranco15243@gmail.com>
2025-04-04 16:48:55 +0200

    Subo cambios ficheros

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import DataProvider from "./context/DataProvider.tsx";
+import logo from "./assets/logo.webp";
+import "./index.css";
+
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <DataProvider>
+        <div className="cosas">
+        <img className="piedra-papel-tijera.gif" src={logo} alt="piedra-papel-tijera.gif" />
+          <h1>PIEDRA PAPEL TIJERA</h1>
+          <img className="piedra-papel-tijera.gif" src={logo} alt="piedra-papel-tijera.gif" />
+        </div>
         <App />
       </DataProvider>
     </BrowserRouter>
